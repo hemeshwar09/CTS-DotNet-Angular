93ec525dd932706b599aaf0583feadf89f374c1f
Avuthu Hemeshwar Reddy <231fa04a09@gmail.com>
2026-07-24 11:12:17 +0530

    Delete CTS-Angular directory

--- CTS-Angular/DeepSkilling/week-7/Module 7 - Microservices Architecture using ASP.NET Core Web API/Validate and Handle Unauthorized Access/JWTUnauthorizedAPI/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-namespace JWTUnauthorizedAPI.Models
-{
-    public class LoginModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
